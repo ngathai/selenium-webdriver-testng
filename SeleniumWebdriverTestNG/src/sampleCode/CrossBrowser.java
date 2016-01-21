e2a4f61e2507e611d932da99441d0a46e1957411
ThaiAnhNga <nga.thai@enclave.vn>
2016-01-21 09:53:55 +0700

    Add class cross browser

---
+++ SeleniumWebdriverTestNG/src/sampleCode/CrossBrowser.java
@@ -0,0 +1,58 @@
+package sampleCode;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class CrossBrowser {
+	WebDriver driver;
+	String url = "http://uat2.theluxurycloset.com/lykan/web/";
+// Config running in xml: testCrossBrowser.xml
+	@BeforeTest
+	@Parameters("browser")
+	public void beforeTest(String browser) {
+		if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", "//home//enclaveit//Desktop//chromedriver");
+			driver = new ChromeDriver();
+		}
+		driver.get(url);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.close();
+	}
+
+	@Test(dataProvider = "LoginProvider")
+	public void crossBrowser(String username, String password) throws InterruptedException {
+		WebElement btnLoginHeader = driver.findElement(By.className("login-link"));
+		btnLoginHeader.click();
+		Thread.sleep(5000);
+		WebElement txtEmail = driver.findElement(By.className("user-email"));
+		txtEmail.sendKeys(username);
+		WebElement txtPass = driver.findElement(By.className("user-pwd"));
+		txtPass.sendKeys(password);
+		WebElement btnLogin = driver.findElement(By.id("login_btn"));
+		btnLogin.click();
+		Thread.sleep(6000);
+		WebElement myacc = driver.findElement(By.className("My-account"));
+		String expected = "MY ACCOUNT";
+		String actual = myacc.getText();
+		Assert.assertEquals(actual, expected);
+	}
+	
+	@org.testng.annotations.DataProvider(name = "LoginProvider")
+	public Object[][] getDataFromDataprovider() {
+		return new Object[][] { { "thaithianhnga@gmail.com", "123456789" } };
+	}
+}
